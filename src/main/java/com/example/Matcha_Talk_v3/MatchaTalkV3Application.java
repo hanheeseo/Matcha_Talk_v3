28301c76cd45edf7060f07c83fca083db38f98ae
hanheeseo <shh06272@gmail.com>
2025-09-26 16:06:36 +0900

    first commit

---
+++ src/main/java/com/example/Matcha_Talk_v3/MatchaTalkV3Application.java
@@ -0,0 +1,13 @@
+package com.example.Matcha_Talk_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchaTalkV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchaTalkV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Matcha_Talk_v3/MatchaTalkV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Matcha_Talk_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchaTalkV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
